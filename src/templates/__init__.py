e1c863eb362c804afc55c12634ec0435602585bf
Ivan Carvalho <ivancarv@student.ubc.ca>
2023-04-01 09:14:08 -0700

    Start adding template for Alta

--- src/templates/__init__.py
+++ src/templates/__init__.py
@@ -8,6 +8,7 @@ template_commands = {
     "Deedy": ["xelatex", "-interaction=nonstopmode", "resume.tex"],
     "Modern": ["pdflatex", "-interaction=nonstopmode", "resume.tex"],
     "Plush": ["xelatex", "-interaction=nonstopmode", "resume.tex"],
+    #"Alta": ["xelatex", "-interaction=nonstopmode", "resume.tex"],
 }
 
 
