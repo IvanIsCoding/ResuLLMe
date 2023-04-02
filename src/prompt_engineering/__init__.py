e4a793fc2e64df8a358c530b174c3dbf5010c413
Ivan Carvalho <ivancarv@student.ubc.ca>
2023-04-02 12:43:55 -0700

    Add awards prompt

--- src/prompt_engineering/__init__.py
+++ src/prompt_engineering/__init__.py
@@ -29,3 +29,31 @@ Now consider the following JSON schema:
 
 Write the education section according to the schema
 """
+
+awards_prompt = """
+You are a career adviser at the Harvard Extension School. You are going to write a JSON resume section for an applicant applying for the following job post:
+{JOB_POST_DESCRIPTION}
+
+Consider the following CV:
+{CV_TEXT}
+
+Now consider the following JSON schema:
+{
+    "awards": [
+        {
+            "title": "TreeHacks 2019 Best Future of Education Hack",
+            "date": "February, 2019",
+            "awarder": "Stanford University",
+            "summary": "Our project to match students from the Los Angeles School District with mentors at Ivy League schools won the award."
+        },
+        {
+            "title": "HackTech 2018 Best Retro Hack",
+            "date": "July 2018",
+            "awarder": "California Institute of Technology",
+            "summary": "Our project to create a virtual reality version of the 1980s arcade game 'Pong' won the award."
+        }
+    ]
+}
+
+Write the awards section according to the schema. Include only the awards section.
+"""
